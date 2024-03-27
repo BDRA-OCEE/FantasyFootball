c6043dca57c463957a2ad2c9e66c46c12112441e
svywx <svywxz@gmail.com>
2024-03-27 20:56:18 +0200

    Initial Commit

---
+++ src/test/java/Module3Project/FantasyFootball/FantasyFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package Module3Project.FantasyFootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
