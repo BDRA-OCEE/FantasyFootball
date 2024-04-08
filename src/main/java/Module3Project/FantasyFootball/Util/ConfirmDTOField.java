70544a5044f411c7d9ff3a86ade252f8a4e664d7
svywx <svywxz@gmail.com>
2024-04-08 17:35:56 +0300

    Initial Project Structuring + more commit & push testing

---
+++ src/main/java/Module3Project/FantasyFootball/Util/ConfirmDTOField.java
@@ -0,0 +1,7 @@
+package Module3Project.FantasyFootball.Util;
+
+public final class ConfirmDTOField {
+    public static boolean confirmDTOField(String name, String confirmName){
+        return name.equals(confirmName);
+    }
+}
