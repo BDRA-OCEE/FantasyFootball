1cf878f60d67a7a0a5a988d45f8b124b46358cf4
svywx <svywxz@gmail.com>
2024-04-09 18:54:43 +0300

    Additional user setup

--- src/main/java/Module3Project/FantasyFootball/Auth/UserRepository.java
+++ src/main/java/Module3Project/FantasyFootball/Auth/UserRepository.java
@@ -3,4 +3,7 @@ package Module3Project.FantasyFootball.Auth;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User getUserByUsername(String username);
+
+    User getUserByEmail(String username);
 }
