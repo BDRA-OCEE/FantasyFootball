9de3b25814393f27f27da9fd8bb836e7d5ad37a0
svywx <svywxz@gmail.com>
2024-04-11 17:06:18 +0300

    building classes.

--- src/main/java/Module3Project/FantasyFootball/FootballApi/FootballApiController.java
+++ src/main/java/Module3Project/FantasyFootball/FootballApi/FootballApiController.java
@@ -1,11 +1,6 @@
 package Module3Project.FantasyFootball.FootballApi;
 
-import Module3Project.FantasyFootball.FootballApi.TestApis.RandomFunFactDTO;
-import org.springframework.boot.system.ApplicationPid;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
